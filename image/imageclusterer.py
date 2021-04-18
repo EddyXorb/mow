5090d19c18b2a6aa053f9170c9949c364b1638b5
eddyxorb <hans-peter.gmuerr@freenet.de>
2021-04-18 12:14:02 +0200

    Basic imageclusterer class

--- image/imageclusterer.py
+++ image/imageclusterer.py
@@ -4,55 +4,121 @@ import os
 import datetime as dt
 import numpy as np
 from typing import List
+from collections import defaultdict
 
-from imagehelper import getExifDateFrom
-from imagefile import ImageFile
+from .imagehelper import getExifDateFrom
+from .imagefile import ImageFile
+from general.verboseprinterclass import VerbosePrinterClass
 
 
 class DateImageFile(ImageFile):
     def __init__(self, file):
         super().__init__(file)
         if self.isValid():
-            self.date = getExifDateFrom(self.getJpg())
+            self.date: dt.datetime = getExifDateFrom(self.getJpg())
             self.baseTime: dt.datetime = None
 
 
-#%%
-testfile = "C:\\Users\\claud\\Nextcloud\\Photos\\Fotos\\2021\\_noch_einsortieren\\images\\2020-07-06.T.17.13.41_P7062069.JPG"
-folder = (
-    "C:\\Users\\claud\\Nextcloud\\Photos\\Fotos\\2021\\_noch_einsortieren\\images\\"
-)
+class ImageClusterer(VerbosePrinterClass):
+    """
+    src : full path to folder containing images
+    hoursmaxdiff : cluster
+    move : if true, files are moved instead of copied
+    test : don't copy/move anything, print only files that would be copied.
+    """
 
-targetfolder = os.path.join(folder, "clustered")
-files = os.listdir(folder)
-#%%
-datedFiles: List[DateImageFile] = []
-for file in files:
+    def __init__(
+        self,
+        src: str,
+        hoursmaxdiff: int = 24,
+        move: bool = False,
+        verbose=False,
+        test: bool = False,
+    ):
+        if not os.path.isdir(src):
+            print(src)
+            raise Exception("Given src is not a dir!")
+        if hoursmaxdiff < 1:
+            raise Exception("Hoursmaxdiff is smaller than 1!")
 
-    ifile = DateImageFile(os.path.join(folder, file))
-    if not ifile.isValid():
-        continue
+        self.src = os.path.abspath(src)
+        self.dst = os.path.join(self.src, "clustered")
+        self.secondsmaxdiff = 3600 * hoursmaxdiff
+        self.move = move
+        self.verbose = verbose
+        self.test = test
 
-    datedFiles.append(ifile)
+        self.folderformatstring = "%Y-%m-%dH%H" if hoursmaxdiff < 24 else "%Y-%m-%d"
+        self.files: List[DateImageFile] = []
+        self.nrcluster = 1
+        self.clusterToSize = defaultdict(lambda: int(0))
 
-#%%
-datedFiles.sort(key=lambda x: x.date)
-#%%
-secondsThreshold = 3600 * 48  # two days
-#%%
-lastTime = datedFiles[0].date
-lastBaseTime = datedFiles[0].date
-cntBaseTimes = 0
+        self.collectDateImages()
 
-for im in datedFiles:
-    sec = (im.date - lastTime).total_seconds()
-    if sec > secondsThreshold:
-        lastBaseTime = im.date
-        cntBaseTimes += 1
+        if len(self.files) == 0:
+            print("No files in folder", self.src, ". Do nothing.")
+            return
 
-    lastTime = im.date
-    im.baseTime = lastBaseTime
+        self.fillBaseTimes()
+        self.sortFiles()
+        self.clusterFiles()
+        self.printSummary()
 
-print("Nr cluster found: ", cntBaseTimes)
+    def printSummary(self):
+        print(
+            "Would treat" if self.test else "Treated",
+            len(self.files),
+            "images in",
+            self.nrcluster,
+            "clusters.",
+        )
 
-#%%
+        for cluster, files in self.clusterToSize.items():
+            self.printv("Cluster", cluster, "contains", files, "files.")
+
+    def collectDateImages(self) -> List[DateImageFile]:
+        files = os.listdir(self.src)
+        for file in files:
+            ifile = DateImageFile(os.path.join(self.src, file))
+            if not ifile.isValid():
+                continue
+
+            self.files.append(ifile)
+
+    def sortFiles(self):
+        self.files.sort(key=lambda x: x.date)
+
+    def getClusterNameFrom(self, time: dt.datetime) -> str:
+        return time.strftime(self.folderformatstring)
+
+    def fillBaseTimes(self):
+        lastTime = self.files[0].date
+        lastBaseTime = self.files[0].date
+        self.printv("New cluster", self.getClusterNameFrom(lastBaseTime))
+
+        for im in self.files:
+            sec = (im.date - lastTime).total_seconds()
+            if sec > self.secondsmaxdiff:
+                lastBaseTime = im.date
+                self.nrcluster += 1
+                self.printv("New cluster", self.getClusterNameFrom(lastBaseTime))
+
+            self.clusterToSize[self.getClusterNameFrom(lastBaseTime)] += 1
+            lastTime = im.date
+            im.baseTime = lastBaseTime
+
+    def clusterFiles(self):
+        for im in self.files:
+            folder = im.baseTime.strftime(self.folderformatstring)
+
+            newFolder = os.path.join(self.dst, folder)
+            dst = os.path.join(newFolder, os.path.basename(im.getJpg()))
+
+            self.printv("Would create" if self.test else "Create", "image in", dst)
+            if self.test:
+                continue
+
+            if self.move:
+                im = im.moveTo(dst)
+            else:
+                im.copyTo(dst)
