a3ffe20671cc778900ea216ddee961dfbf1876ac
eddyxorb <hans-peter.gmuerr@freenet.de>
2021-04-18 13:18:56 +0200

    smarter import x_videos.py

--- x_videos.py
+++ x_videos.py
@@ -1,6 +1,6 @@
-import video.transcodeallmovs
+from video.transcodeallmovs import parser,call
 
 if __name__ == "__main__":
     print("Transcode all movie files in folder. Specifiy folder..")
-    args = video.transcodeallmovs.parser.parse_args()
-    video.transcodeallmovs.call(args)
+    args = parser.parse_args()
+    call(args)
