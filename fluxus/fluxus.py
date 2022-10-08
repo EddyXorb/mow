a682a7cc511259d32c826b2bcf6dad69c54d2f37
eddyxorb <hans-peter.gmuerr@freenet.de>
2022-10-08 19:12:13 +0200

    Basic skeleton of fluxus.

---
+++ fluxus/fluxus.py
@@ -0,0 +1,25 @@
+class Fluxus:
+    def __init__(self, settingsfile: str):
+        self.settingsfile = settingsfile
+
+    def copy(self):
+        pass
+
+    def rename(self):
+        pass
+
+    def group(self):
+        pass
+
+    def rate(self):
+        pass
+    
+    def tag(self):
+        pass
+    
+    def localize(self):
+        pass
+    
+    def aggregate(self):
+        pass
+    
