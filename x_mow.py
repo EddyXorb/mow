9991c69928aad47a469b943a07ff1f4b16c39fc1
eddyxorb <hans-peter.gmuerr@freenet.de>
2022-11-06 13:57:48 +0100

    Add simple argparser for renaming only so far.

--- x_mow.py
+++ x_mow.py
@@ -1,4 +1,14 @@
 from modules.mow.mow import Mow
+from argparse import ArgumentParser
+
+parser = ArgumentParser("M(edia) flo(OW) - helper to automate media workflow. Needs a working dir to be specified into .mowsettings.yml.")
+parser.add_argument(
+    "-r", "--rename", help="execute renaming", action="store_true", dest="rename"
+)
 
 if __name__ == "__main__":
-    Mow(".mowsettings.yml").rename()
+    args = parser.parse_args()
+    mow = Mow(".mowsettings.yml")
+
+    if args.rename:
+        mow.rename()
