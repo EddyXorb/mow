9a65310a22ed0ed438317e792ba208f04f24d127
eddyxorb <hans-peter.gmuerr@freenet.de>
2022-11-11 14:27:35 +0100

    Add new convert argument to mow.

--- x_mow.py
+++ x_mow.py
@@ -1,9 +1,15 @@
 from modules.mow.mow import Mow
 from argparse import ArgumentParser
 
-parser = ArgumentParser("M(edia) flo(OW) - helper to automate media workflow. Needs a working dir to be specified into .mowsettings.yml.")
+parser = ArgumentParser(
+    "M(edia) flo(OW) - helper to automate media workflow. Needs a working dir to be specified into .mowsettings.yml."
+)
 parser.add_argument(
     "-r", "--rename", help="execute renaming", action="store_true", dest="rename"
+)
+
+parser.add_argument(
+    "-c", "--convert", help="execute conversion", action="store_true", dest="convert"
 )
 
 if __name__ == "__main__":
@@ -12,3 +18,5 @@ if __name__ == "__main__":
 
     if args.rename:
         mow.rename()
+    if args.convert:
+        mow.convert()
