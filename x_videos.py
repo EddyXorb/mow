bf00aab90ba86fa7a975a73d87b8f3c1cb21b242
eddyxorb <hans-peter.gmuerr@freenet.de>
2022-11-18 23:53:48 +0100

    Deleted obsolete files.

--- x_videos.py
+++
@@ -1,6 +0,0 @@
-from modules.video.transcodeallmovs import parser,call
-
-if __name__ == "__main__":
-    print("Transcode all movie files in folder. Specifiy folder..")
-    args = parser.parse_args()
-    call(args)
