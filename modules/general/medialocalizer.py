e64d7971ede3eeea4d9151484e0f92230832a33b
Claudius Röhl <claudius@roehlnet.de>
2024-09-27 20:39:32 +0200

    Finished unittests for Medialocalizer.

--- modules/general/medialocalizer.py
+++ modules/general/medialocalizer.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import os
 import sys
+import traceback
 from typing import List
 import polars as pl
 import gpxpy
@@ -42,18 +43,19 @@ class LocalizerInput(TransitionerInput):
     recursive : if true, dives into every subdir to look for mediafiles
     dry: don't actually change anything, just show what would be done
     writeMetaTags: sets GPS data in media files
-    time_offset_mediafile_to_gps: timedelta, time offset between mediafile and gps time (e.g. if camera time is 1 hour ahead of gps time, set this to -1 hour).
+    time_offset_mediafile: timedelta, enforced time offset for mediafile to create congruence to gps time (e.g. if camera time is 1 hour ahead of gps time, set this to +1 hour. This the case if we are in Europe/Berlin=UTC+1 (gps is always UTC time)).
     gps_time_tolerance: timedelta, time tolerance for gps data. If no gps data is found within this time range, no gps data will be inserted into the media file.
     mediafile_timezone: str, timezone of the mediafiles. This is used to convert the mediafiles time to the gps time. The mediafile time is taken from the filename, so no metainformation is taken into account (e.g. from Date Time UTC-Flag in JPGs) If the timezone is not given, the timezone "Europe/Berlin" is used.
     force_gps_data: GpsData, if given, this gps data will be inserted into every media file. In this case, all files will be transitioned.
     transition_even_if_no_gps_data: bool, if true, the mediafile will be transitioned even if no gps data was found. In this case, the mediafile will be transitioned without gps data.
     """
 
-    time_offset_mediafile_to_gps: datetime.timedelta = datetime.timedelta(seconds=0)
+    time_offset_mediafile: datetime.timedelta = datetime.timedelta(seconds=0)
     gps_time_tolerance: datetime.timedelta = datetime.timedelta(seconds=60)
     mediafile_timezone: str = "Europe/Berlin"
     force_gps_data: GpsData = None
     transition_even_if_no_gps_data: bool = False
+    print_found_gps_coordinates: bool = False
 
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
@@ -67,13 +69,14 @@ class MediaLocalizer(MediaTransitioner):
         input.mediaFileFactory = createAnyValidMediaFile
         super().__init__(input)
         self.gps_time_tolerance = input.gps_time_tolerance
-        self.time_offset_mediafile_to_gps = input.time_offset_mediafile_to_gps
+        self.time_offset_mediafile = input.time_offset_mediafile
         self.mediafile_timezone = input.mediafile_timezone
         self.force_gps_data = input.force_gps_data
         self.transition_even_if_no_gps_data = input.transition_even_if_no_gps_data
+        self.print_found_gps_coordinates = input.print_found_gps_coordinates
 
         if self.mediafile_timezone not in available_timezones():
-            print(
+            self.printv(
                 f"Timezone {self.mediafile_timezone} is an invalid time zone! Do nothing."
             )
             sys.exit(1)
@@ -94,6 +97,7 @@ class MediaLocalizer(MediaTransitioner):
                 else:
                     mediafile_time = extractDatetimeFromFileName(mediafile.pathnoext)
                     gps_data = self.getGpsDataForTime(mediafile_time)
+
                     if gps_data is None:
                         if self.transition_even_if_no_gps_data:
                             out.append(TransitionTask(index))
@@ -102,14 +106,19 @@ class MediaLocalizer(MediaTransitioner):
                                 TransitionTask(
                                     index,
                                     skip=True,
-                                    skipReason=f"Could not localize {mediafile} because no GPS data was found.",
+                                    skipReason=f"Could not localize {mediafile} because of missing GPS data. File has time {mediafile_time}, which was corrected to {self.getNormalizedMediaFileTime(mediafile_time)}",
                                 )
                             )
-                    out.append(
-                        TransitionTask(
-                            index, metaTags=gps_data.getGPSMetaTagsForWriting()
+                    else:
+                        if self.print_found_gps_coordinates:
+                            self.printv(
+                                f"Found GPS data for {mediafile}:{gps_data}. File has time {mediafile_time}, which was corrected to {self.getNormalizedMediaFileTime(mediafile_time)}"
+                            )
+                        out.append(
+                            TransitionTask(
+                                index, metaTags=gps_data.getGPSMetaTagsForWriting()
+                            )
                         )
-                    )
             except Exception as e:
                 out.append(
                     TransitionTask(
@@ -118,17 +127,21 @@ class MediaLocalizer(MediaTransitioner):
                         skipReason=f"Could not localize {mediafile} because of {e}",
                     )
                 )
-        print(out)
+
         return out
 
     def getAllGpxFiles(self) -> List[str]:
         all_files = os.listdir(self.src)
-        return [f for f in all_files if f.endswith(".gpx")]
+        return [
+            os.path.abspath(os.path.join(self.src, f))
+            for f in all_files
+            if f.endswith(".gpx")
+        ]
 
     def getAllPositionsDataframe(self) -> pl.DataFrame:
         gpx_files = self.getAllGpxFiles()
-
         file_to_gpx_data = {}
+
         for gpx_filepath in gpx_files:
             with open(gpx_filepath, "r") as gpx_file:
                 file_to_gpx_data[gpx_filepath] = gpxpy.parse(gpx_file)
@@ -157,16 +170,12 @@ class MediaLocalizer(MediaTransitioner):
     def getGpsDataForTime(
         self,
         mediafile_time: datetime.datetime,
-        time_offset: datetime.timedelta = datetime.timedelta(seconds=0),
-        gps_time_tolerance: datetime.timedelta = datetime.timedelta(seconds=60),
     ) -> GpsData:
 
-        mediafile_time_in_gps_time = self.getNormalizedMediaFileTime(
-            mediafile_time, time_offset
-        )
+        mediafile_time_in_gps_time = self.getNormalizedMediaFileTime(mediafile_time)
 
         before_position, after_position = self.getBeforeAfterGpsData(
-            gps_time_tolerance, mediafile_time_in_gps_time
+            self.gps_time_tolerance, mediafile_time_in_gps_time
         )
 
         if before_position is not None and after_position is not None:
@@ -178,7 +187,6 @@ class MediaLocalizer(MediaTransitioner):
         elif after_position is not None:
             return after_position
         else:
-            print(f"No GPS data found for {mediafile_time_in_gps_time}")
             return None
 
     def getInterpolatedGpsData(
@@ -200,13 +208,16 @@ class MediaLocalizer(MediaTransitioner):
         )
 
     def getNormalizedMediaFileTime(self, mediafile_time) -> datetime.datetime:
-        mediafile_time_in_gps_time = mediafile_time + self.time_offset_mediafile_to_gps
+        mediafile_time_in_gps_time = mediafile_time + self.time_offset_mediafile
 
         if mediafile_time.tzinfo is None:
-            mediafile_time_in_gps_time = mediafile_time.replace(
+            mediafile_time_in_gps_time = mediafile_time_in_gps_time.replace(
                 tzinfo=ZoneInfo(self.mediafile_timezone)
-            ).astimezone(ZoneInfo(INTERNAL_BASE_TIMEZONE))
+            )
 
+        mediafile_time_in_gps_time = mediafile_time_in_gps_time.astimezone(
+            ZoneInfo(INTERNAL_BASE_TIMEZONE)
+        )
         return mediafile_time_in_gps_time
 
     def getBeforeAfterGpsData(
--- tests/test_localizer.py
+++ tests/test_localizer.py
@@ -1,3 +1,4 @@
+import datetime
 import shutil
 from os.path import join, exists
 import os
@@ -11,31 +12,22 @@ testfolder = "tests"
 src = os.path.abspath(join(testfolder, "filestotreat"))
 dst = os.path.abspath("./tests/test_treated")
 targetDir = join(dst, "subsubfolder")
-imagename = "test3.JPG"
-srcfile = join(src, "subsubfolder", imagename)
-expectedTransitionedFile = join(dst, "subsubfolder", imagename)
+IMAGENAME = "2022-01-01@101015.JPG"
+
+untransitionedFile = join(src, "subsubfolder", IMAGENAME)
+transitionedFile = join(dst, "subsubfolder", IMAGENAME)
 
 
-# def executeConversionWith(deleteOriginals=True, maintainFolderStructure=True):
-#     MediaLocalizer(
-#         LocalizerInput(
-#             src=src,
-#             dst=dst,
-#             deleteOriginals=deleteOriginals,
-#             maintainFolderStructure=maintainFolderStructure,
-
-#         )
-#     )()
-
-
-def prepareTest():
+def prepareTest(image_name=IMAGENAME):
     shutil.rmtree(src, ignore_errors=True)
     shutil.rmtree(dst, ignore_errors=True)
-    os.makedirs(os.path.dirname(srcfile))
+    os.makedirs(os.path.dirname(untransitionedFile))
     shutil.copy(
         join(testfolder, "test3.JPG"),
-        srcfile,
+        join(src, "subsubfolder", image_name),
     )
+    if not os.path.exists(join(src, "test.gpx")):
+        shutil.copy(join(testfolder, "test.gpx"), join(src, "test.gpx"))
 
 
 def test_force_gps_works():
@@ -51,14 +43,265 @@ def test_force_gps_works():
         )
     )()
 
-    assert not exists(srcfile)
-    assert exists(expectedTransitionedFile)
+    assert not exists(untransitionedFile)
+    assert exists(transitionedFile)
 
     with ExifToolHelper() as et:
-        tags = et.get_tags(expectedTransitionedFile, MowTags.gps_all, params=["-n"])[
+        tags = et.get_tags(transitionedFile, MowTags.gps_all, params=["-n"])[
             0
         ]  # -n formats the gps output as decimal numbers
         print(tags)
         assert tags[MowTags.gps_latitude] == 1.0
         assert tags[MowTags.gps_longitude] == -2.0
         assert tags[MowTags.gps_elevation_read_only] == 3.0
+
+
+# The gpx data in test-gpx looks like this:
+#
+# <trkseg>
+#   <trkpt lat="10" lon="-10">
+#     <ele>1000</ele>
+#     <time>2022-01-01T10:10:10Z</time>
+#   </trkpt>
+#   <trkpt lat="20" lon="10">
+#     <ele>2000</ele>
+#     <time>2022-01-01T10:10:20Z</time>
+#   </trkpt>
+# </trkseg>
+
+
+def test_no_gps_data_leads_to_no_transition():
+    image = "2099-11-11@444444.JPG"
+    prepareTest(image_name=image)
+    untransitionedFile = join(src, "subsubfolder", image)
+    transitionedFile = join(dst, "subsubfolder", image)
+
+    MediaLocalizer(
+        LocalizerInput(
+            src=src,
+            dst=dst,
+            deleteOriginals=True,
+            maintainFolderStructure=True,
+        )
+    )()
+
+    assert exists(untransitionedFile)
+    assert not exists(transitionedFile)
+
+
+def test_correct_gps_data_leads_to_transition_and_interpolation():
+    prepareTest()
+
+    MediaLocalizer(
+        LocalizerInput(
+            src=src,
+            dst=dst,
+            deleteOriginals=True,
+            maintainFolderStructure=True,
+            mediafile_timezone="UTC",
+        )
+    )()
+    assert not exists(untransitionedFile)
+    assert exists(transitionedFile)
+
+    with ExifToolHelper() as et:
+        tags = et.get_tags(transitionedFile, MowTags.gps_all)[
+            0
+        ]  # -n formats the gps output as decimal numbers
+        print(tags)
+        assert tags[MowTags.gps_latitude] == 15
+        assert tags[MowTags.gps_longitude] == 0
+        assert tags[MowTags.gps_elevation_read_only] == 1500
+
+
+def test_timezone_change_leads_to_no_gps_found():
+    prepareTest()
+
+    MediaLocalizer(
+        LocalizerInput(
+            src=src,
+            dst=dst,
+            deleteOriginals=True,
+            maintainFolderStructure=True,
+            mediafile_timezone="Europe/Berlin",
+        )
+    )()
+
+    assert exists(untransitionedFile)
+    assert not exists(transitionedFile)
+
+
+def test_time_image_offset_works():
+    prepareTest()
+
+    MediaLocalizer(
+        LocalizerInput(
+            src=src,
+            dst=dst,
+            deleteOriginals=True,
+            maintainFolderStructure=True,
+            mediafile_timezone="Europe/Berlin",  # +1 hours offset to UTC
+            time_offset_mediafile=datetime.timedelta(
+                hours=+1
+            ),  # here we add the offset again
+        )
+    )()
+
+    assert not exists(untransitionedFile)
+    assert exists(transitionedFile)
+
+
+def test_transition_even_if_no_gps_data_works():
+    prepareTest()
+
+    MediaLocalizer(
+        LocalizerInput(
+            src=src,
+            dst=dst,
+            deleteOriginals=True,
+            maintainFolderStructure=True,
+            mediafile_timezone="Europe/Berlin",  # +1 hours offset to UTC
+            transition_even_if_no_gps_data=True,  # here we add the offset again
+        )
+    )()
+
+    assert not exists(untransitionedFile)
+    assert exists(transitionedFile)
+
+
+def test_gps_time_tolerance_when_too_small_avoids_transition():
+    prepareTest()
+
+    MediaLocalizer(
+        LocalizerInput(
+            src=src,
+            dst=dst,
+            deleteOriginals=True,
+            maintainFolderStructure=True,
+            mediafile_timezone="UTC",
+            gps_time_tolerance=datetime.timedelta(seconds=4),
+        )
+    )()
+
+    assert exists(untransitionedFile)
+    assert not exists(transitionedFile)
+
+
+def test_gps_time_tolerance_when_big_enough_transitions():
+    prepareTest()
+
+    MediaLocalizer(
+        LocalizerInput(
+            src=src,
+            dst=dst,
+            deleteOriginals=True,
+            maintainFolderStructure=True,
+            mediafile_timezone="UTC",
+            gps_time_tolerance=datetime.timedelta(seconds=5),
+        )
+    )()
+
+    assert not exists(untransitionedFile)
+    assert exists(transitionedFile)
+
+
+def test_image_made_before_first_gps_entry_works():
+    image = "2022-01-01@101005.JPG"
+    prepareTest(image_name=image)
+    untransitionedFile = join(src, "subsubfolder", image)
+    transitionedFile = join(dst, "subsubfolder", image)
+
+    MediaLocalizer(
+        LocalizerInput(
+            src=src,
+            dst=dst,
+            deleteOriginals=True,
+            maintainFolderStructure=True,
+            mediafile_timezone="UTC",
+            gps_time_tolerance=datetime.timedelta(seconds=10),
+        )
+    )()
+
+    assert not exists(untransitionedFile)
+    assert exists(transitionedFile)
+
+    with ExifToolHelper() as et:
+        tags = et.get_tags(transitionedFile, MowTags.gps_all)[
+            0
+        ]  # -n formats the gps output as decimal numbers
+        print(tags)
+        assert tags[MowTags.gps_latitude] == 10
+        assert tags[MowTags.gps_longitude] == -10
+        assert tags[MowTags.gps_elevation_read_only] == 1000
+
+
+def test_image_made_before_first_gps_entry_but_too_small_tolerance_does_not_transition():
+    image = "2022-01-01@101005.JPG"
+    prepareTest(image_name=image)
+    untransitionedFile = join(src, "subsubfolder", image)
+    transitionedFile = join(dst, "subsubfolder", image)
+
+    MediaLocalizer(
+        LocalizerInput(
+            src=src,
+            dst=dst,
+            deleteOriginals=True,
+            maintainFolderStructure=True,
+            mediafile_timezone="UTC",
+            gps_time_tolerance=datetime.timedelta(seconds=2),
+        )
+    )()
+
+    assert exists(untransitionedFile)
+    assert not exists(transitionedFile)
+
+
+def test_image_made_after_last_gps_entry_works():
+    image = "2022-01-01@101025.JPG"
+    prepareTest(image_name=image)
+    untransitionedFile = join(src, "subsubfolder", image)
+    transitionedFile = join(dst, "subsubfolder", image)
+
+    MediaLocalizer(
+        LocalizerInput(
+            src=src,
+            dst=dst,
+            deleteOriginals=True,
+            maintainFolderStructure=True,
+            mediafile_timezone="UTC",
+            gps_time_tolerance=datetime.timedelta(seconds=5),
+        )
+    )()
+
+    assert not exists(untransitionedFile)
+    assert exists(transitionedFile)
+
+    with ExifToolHelper() as et:
+        tags = et.get_tags(transitionedFile, MowTags.gps_all)[
+            0
+        ]  # -n formats the gps output as decimal numbers
+        print(tags)
+        assert tags[MowTags.gps_latitude] == 20
+        assert tags[MowTags.gps_longitude] == 10
+        assert tags[MowTags.gps_elevation_read_only] == 2000
+
+
+def test_image_made_after_last_gps_entry_but_too_small_tolerance_does_not_transition():
+    image = "2022-01-01@101025.JPG"
+    prepareTest(image_name=image)
+    untransitionedFile = join(src, "subsubfolder", image)
+    transitionedFile = join(dst, "subsubfolder", image)
+
+    MediaLocalizer(
+        LocalizerInput(
+            src=src,
+            dst=dst,
+            deleteOriginals=True,
+            maintainFolderStructure=True,
+            mediafile_timezone="UTC",
+            gps_time_tolerance=datetime.timedelta(seconds=2),
+        )
+    )()
+
+    assert exists(untransitionedFile)
+    assert not exists(transitionedFile)
